2abfb4f75edb3ac71b47443037ed482a5a107b0e
chasefarmer2808 <chasefarmer94@gmail.com>
2024-03-09 10:47:51 -0500

    first customizations

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  author: "Chase Farmer",
+  desc: "A blog site for Chase Farmer",
+  title: "Chase Farmer",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
@@ -26,7 +26,7 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/chasefarmer2808",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
@@ -34,17 +34,17 @@ export const SOCIALS: SocialObjects = [
     name: "Facebook",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Facebook`,
-    active: true,
+    active: false,
   },
   {
     name: "Instagram",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Instagram`,
-    active: true,
+    active: false,
   },
   {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://www.linkedin.com/in/chase-farmer-236b82115/",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
---
+++ src/content/blog/first-post.md
@@ -0,0 +1,5 @@
+---
+title: "Welcome to my blog"
+pubDatetime: 2024-03-09T00:00:00Z
+description: "Blog intro page."
+---
