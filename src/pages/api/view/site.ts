5c485644aa5c34e7dd4bc1b0070906dcd1d1897d
chasefarmer2808 <chasefarmer94@gmail.com>
2024-03-10 18:12:48 -0400

    kv factory and inmemory client

--- src/pages/api/view/site.ts
+++ src/pages/api/view/site.ts
@@ -1,15 +1,12 @@
 import type { APIRoute } from "astro";
-import { createClient } from "@vercel/kv";
+import { getKvClient } from "@utils/kvClient";
 
-const { KV_REST_API_URL, KV_REST_API_TOKEN } = import.meta.env;
+const { ENVIRONMENT } = import.meta.env;
 
-const kv = createClient({
-  url: KV_REST_API_URL,
-  token: KV_REST_API_TOKEN,
-});
+const kv = getKvClient(ENVIRONMENT);
 
 export const GET: APIRoute = async () => {
-  const viewCount = await kv.hget<number>("site", "views");
+  const viewCount = await kv.get<number>("site", "views");
 
   return new Response(JSON.stringify({ views: viewCount }), { status: 200 });
 };
---
+++ src/utils/kvClient.ts
@@ -0,0 +1,97 @@
+import type { Env } from "types";
+import { VercelKV, createClient } from "@vercel/kv";
+
+const { KV_REST_API_URL, KV_REST_API_TOKEN } = import.meta.env;
+
+export interface KvStore {
+  site: KvSite;
+}
+
+export interface KvSite {
+  views: number;
+}
+
+export interface KvClient {
+  get<T>(key: keyof KvStore, field: string): Promise<T | null>;
+  increment(key: keyof KvStore, field: string, by: number): Promise<number>;
+}
+
+export function getKvClient(env: Env): KvClient {
+  switch (env) {
+    case "prd":
+      return new VercelKvClient();
+
+    default:
+      return new InMemoryKvClient();
+      break;
+  }
+}
+
+class VercelKvClient implements KvClient {
+  client: VercelKV;
+
+  constructor() {
+    this.client = createClient({
+      url: KV_REST_API_URL,
+      token: KV_REST_API_TOKEN,
+    });
+  }
+
+  async get<T>(key: keyof KvStore, field: string): Promise<T | null> {
+    return this.client.hget<T>(key, field);
+  }
+
+  async increment(
+    key: keyof KvStore,
+    field: string,
+    by: number
+  ): Promise<number> {
+    return this.client.hincrby(key, field, by);
+  }
+}
+
+class InMemoryKvClient implements KvClient {
+  store: KvStore;
+
+  constructor() {
+    this.store = {
+      site: {
+        views: 0,
+      },
+    };
+  }
+
+  async get<T>(key: keyof KvStore, field: string): Promise<T | null> {
+    return new Promise((res, rej) => {
+      if (Object.keys(this.store).includes(key)) {
+        const keyStore = this.store[key];
+        const fieldType = field as keyof typeof keyStore;
+
+        res(this.store[key][fieldType] as T);
+      } else {
+        rej(null);
+      }
+    });
+  }
+
+  async increment(
+    key: keyof KvStore,
+    field: string,
+    by: number
+  ): Promise<number> {
+    return new Promise((res, rej) => {
+      if (Object.keys(this.store).includes(key)) {
+        const keyStore = this.store[key];
+        const fieldType = field as keyof typeof keyStore;
+        const currVal = this.store[key][fieldType];
+        const newVal = currVal + by;
+
+        this.store[key][fieldType] = newVal;
+
+        res(newVal);
+      } else {
+        rej(0);
+      }
+    });
+  }
+}
