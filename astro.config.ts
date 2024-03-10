3d7ea9ed88b26ca75e185daa073e6b9dc2fe00df
chasefarmer2808 <chasefarmer94@gmail.com>
2024-03-10 19:30:04 -0400

    trying vercel serverless'

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,7 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -40,5 +40,5 @@ export default defineConfig({
   },
   scopedStyleStrategy: "where",
   output: "hybrid",
-  adapter: vercel(),
+  adapter: vercelServerless(),
 });
